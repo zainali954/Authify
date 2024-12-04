30f09ed086ff7ba860d357b5a58172964d900c6b
zainali954 <105zainali@gmail.com>
2024-12-04 11:51:55 +0500

    ready for production

--- backend/app.js
+++ backend/app.js
@@ -5,12 +5,13 @@ import authRoutes from './routes/auth.routes.js';
 import userRoutes from './routes/user.routes.js'
 import adminRoutes from './routes/admin/admin.routes.js'
 import apiResponse from './utils/apiResponse.js';
-
+import path from 'path'
 const app = express();
 
 // Middleware to parse JSON
 app.use(express.json());
 
+const _dirname = path.resolve()
 
 app.use(cors({
     origin : process.env.CLIENT_URL,
@@ -24,8 +25,13 @@ app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/user', userRoutes);
 app.use('/api/v1/admin', adminRoutes)
 
+
+app.use(express.static(path.join(_dirname, "frontend/dist")))
+app.get('*', (_, res)=>{
+    res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"))
+})
 // Global error handling middleware
-app.use((err, req, res, next) => {
+app.use((err, req, res, _) => {
     console.error('Error:', err); // Log the error for debugging
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
